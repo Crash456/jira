95b51e19374ed0afc330829bfd857a212e6d190b
Asher <784904811@qq.com>
2025-02-18 13:27:45 +0800

    ts修改

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import qs from "qs";
+import { cleanObject, useMount, useDebounce } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 200);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`,
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel
+        param={param}
+        setParam={setParam}
+        users={users}
+      ></SearchPanel>
+      <List users={users} list={list}></List>
+    </div>
+  );
+};
